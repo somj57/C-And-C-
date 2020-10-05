d97fc5a6fa7d89115c3311cba3d661b84af415c5
Somya Jaiswal <59413394+somj57@users.noreply.github.com>
2020-10-05 08:40:21 +0530

    Adding files

---
+++ CLOCK.C
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int i,j;
+ clrscr();
+ for(i=1;i<=30;i++)
+ {
+
+   clrscr();
+   printf("\n********");
+   printf("\n********");
+   printf("\n ****** ");
+   printf("\n  ****"  );
+   printf("\n   **   ");
+   printf("\n  *  *  ");
+   printf("\n *    *" );
+   printf("\n*      *");
+   printf("\n********");
+   delay(600);
+   clrscr();
+   printf("\n********");
+   printf("\n*      *");
+   printf("\n *    * ");
+   printf("\n  *  *  ");
+   printf("\n   **   ");
+   printf("\n  ****  ");
+   printf("\n ******" );
+   printf("\n********");
+   printf("\n********");
+   delay(600);
+   clrscr();
+   }
+   getch();
+ }
